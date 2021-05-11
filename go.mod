21bdc30b558f06702021ed092b97dee1d8938c74
hiroki <hiroki@rhems-japan.co.jp>
2021-05-11 16:42:16 +0900

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/RHEMS-Japan/blue-deploy-ext
+
+go 1.14
+
+require (
+	github.com/go-ldap/ldap/v3 v3.3.0 // indirect
+	github.com/jtblin/go-ldap-client v0.0.0-20170223121919-b73f66626b33
+	gopkg.in/asn1-ber.v1 v1.0.0-20181015200546-f715ec2f112d // indirect
+	gopkg.in/ldap.v2 v2.5.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jtblin/go-ldap-client"
+)
+
+var key string
+
+func main() {
+
+	base := os.Getenv("LDAP_Base")
+	host := os.Getenv("LDAP_Host")
+	user := os.Getenv("LDAP_User")
+	pass := os.Getenv("LDAP_Pass")
+
+	client := &ldap.LDAPClient{
+		Base:        base,
+		Host:        host,
+		Port:        389,
+		UseSSL:      false,
+		SkipTLS:     true,
+		BindDN:      fmt.Sprintf("uid=readonlysuer,ou=People,%s", base),
+		UserFilter:  "(uid=%s)",
+		GroupFilter: "(memberUid=%s)",
+		Attributes:  []string{"givenName", "sn", "mail", "uid"},
+	}
+
+	defer client.Close()
+
+	client.ServerName = host
+
+	ok, _, err := client.Authenticate(user, pass)
+	if err != nil {
+		log.Fatalf("auth error , user %s: error %+v", user, err)
+	}
+	if !ok {
+		log.Fatalf("auth failed for user %s", user)
+	}
+	if key == "" {
+		fmt.Println("key is not setting")
+	} else {
+		fmt.Print(key)
+	}
+}
